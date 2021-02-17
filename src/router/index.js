d019c6f4b7f5cdf03a36e99cd5f5d2e68b61db46
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-02-17 12:13:55 +0700

    add bottom tab navigator

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,26 @@ import {StyleSheet, Text, View} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Feed, GetStarted, MainPage, NamePage, Setting} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-export const MyDrawer = () => {
+const MyTabBottom = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="MainPage" component={MainPage} />
+      <Tab.Screen name="Setting" component={Setting} />
+      <Tab.Screen name="Feed" component={Feed} />
+    </Tab.Navigator>
+  );
+};
+
+const MyDrawer = () => {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="MainPage" component={MainPage} />
-      <Drawer.Screen name="Setting" component={Setting} />
-      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="MyTabBottom" component={MyTabBottom} />
     </Drawer.Navigator>
   );
 };
@@ -38,6 +48,11 @@ const Router = () => {
       <Stack.Screen
         name="MyDrawer"
         component={MyDrawer}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MyTabBottom"
+        component={MyTabBottom}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
